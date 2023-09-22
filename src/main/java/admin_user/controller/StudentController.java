977e560c24138d10751f960f5f25f6a736a5b574
Nightmar1e <walidelghalidi2001@gmail.com>
2023-09-22 14:59:57 +0200

    student works for admin-page

--- src/main/java/admin_user/controller/StudentController.java
+++ src/main/java/admin_user/controller/StudentController.java
@@ -40,7 +40,7 @@ public class StudentController {
     @PostMapping("/students")
     public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
-        return "redirect:/students";
+        return "redirect:/admin-page/students";
     }
 
     @GetMapping("/students/edit/{id}")
@@ -63,7 +63,7 @@ public class StudentController {
 
         // save updated student object
         studentService.updateStudent(existingStudent);
-        return "redirect:/students";
+        return "redirect:/admin-page/students";
     }
 
     // handler method to handle delete student request
@@ -71,7 +71,7 @@ public class StudentController {
     @GetMapping("/students/{id}")
     public String deleteStudent(@PathVariable Long id) {
         studentService.deleteStudentById(id);
-        return "redirect:/students";
+        return "redirect:/admin-page/students";
     }
 
 }
